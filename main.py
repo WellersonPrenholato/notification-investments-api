aef81bf5e44fd38632d14b35afb86604d8c7d5e0
WellersonPrenholato <wellerson.prenholato@gmail.com>
2023-03-20 01:52:13 -0300

    Construção arquivo main.py

--- main.py
+++ main.py
@@ -1,49 +1,15 @@
-import requests
-from bs4 import BeautifulSoup
-
-
-def union_dict_list (fii_values:list):
-    dict_payload = {
-            "Liquidez Diária":0,
-            "Último Rendimento":0,
-            "Dividend Yield":0,
-            "Patrimônio Líquido":0,
-            "Valor Patrimonial":0,
-            "Rentab. no mês":0,
-            "P/VP":0
-        }
-
-    for key, value in zip(dict_payload.keys(), fii_values):
-        dict_payload[key] = value
-        # print(f"{key}:{value}")
-        
-    return dict_payload
-
-def get_fii_quote(fii_id:str):
-    # headers = {
-    #     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36',
-    #     'Referer': 'https://www.google.com'
-    # }
-    
-    url = f'https://www.fundsexplorer.com.br/funds/{fii_id}'
-    response = requests.get(url)
-    print(response)
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    quote_element = []
-    for item in soup.find_all('span', attrs={'class': 'indicator-value'}):
-        quote_element.append(item.text.replace('\n', ' ').strip())
-        
-    return quote_element
-
-def imprime_dict(dict_values: dict):
-    for chave, valor in dict_values.items():
-        print(chave, ":", valor)
+from yahoo_finance import YahooFinances
+from funds_explorer import FundsExplorer
 
 if __name__ == "__main__":
-
-    fii_string = input("Insira o identificador do FII: ")
-    dict_a = {}
-    quote = get_fii_quote(fii_string)
-    dict_a = union_dict_list(quote)
-    imprime_dict(dict_a)
+    
+    name_fii = 'mxrf11'
+    
+    investiment_yahoo = YahooFinances(name_fii)
+    investiment_yahoo.make_get_fii_quote()
+    
+    print('*' * 30)
+    
+    investiment_funds_explorer = FundsExplorer(name_fii)
+    investiment_funds_explorer.make_get_fii_quote()
+    
